0b84aa520c03928dfc3b29b1b89da607dced082e
Shivam <kumarshivam3788@gmail.com>
2025-01-15 03:19:46 +0530

    Add dotenv package and configure Prisma to use environment variable for database URL

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv';
 
+dotenv.config();
 // Define a global object to store Prisma instance
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
@@ -7,7 +9,13 @@ const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   (() => {
-    const prismaClient = new PrismaClient();
+    const prismaClient = new PrismaClient({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL, // Use the loaded environment variable
+        },
+      },
+    });
     // Store the instance globally in development mode
     if (process.env.NODE_ENV !== "production") {
       globalForPrisma.prisma = prismaClient;
